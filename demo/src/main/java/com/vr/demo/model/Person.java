2c8c8156933112478bb644699b7dfd0071345e97
vr <alviirappu@gmail.com>
2020-07-03 17:07:02 +0700

    Ubah kembali Person.java

--- demo/src/main/java/com/vr/demo/model/Person.java
+++ demo/src/main/java/com/vr/demo/model/Person.java
@@ -7,16 +7,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Person {
 	private final UUID id;
 	private final String name;
-	private final String address;
-	
 	
 	public Person(@JsonProperty("id") UUID id, 
-				  @JsonProperty("name") String name,
-				  @JsonProperty("address") String address) {
+				  @JsonProperty("name") String name) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.address = address;
 	}
 	public UUID getId() {
 		return id;
