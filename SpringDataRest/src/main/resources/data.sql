ff53701042cd5124c7828d596e44eb7ddb2a77d9
alviraputerii <50914858+alviraputerii@users.noreply.github.com>
2020-07-03 16:52:24 +0700

    Add files via upload

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'Nicholas','Java');
+insert into alien values (102,'Ignacio','Python');
+insert into alien values (103,'Voyya','C++');
+insert into alien values (104,'Cio','Java');
+insert into alien values (105,'Nic','Python');
